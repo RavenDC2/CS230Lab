226a9ecfc50506cd4deb53fa8b6b28e77e173988
RavenDC2 <dr00002@mix.wvu.edu>
2021-02-05 20:34:36 -0500

    Lab2:About Page

---
+++ index.php
@@ -0,0 +1,50 @@
+<main>
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+        <h1>About Us</h1>
+        <p>Get to Know Our Team!</p>
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/bluejay.jpeg">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p>Blu Jay</p>
+                    <p>Email: BJayBird@birdhouse.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/raven.jpeg">
+                <div class="container">
+                    <h2>Interior Design Specialist</h2>
+                    <p>Klaus Raven</p>
+                    <p>Email: Klaus.Raven@birdhouse.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/robin.jpg">
+                <div class="container">
+                    <h2>CMO</h2>
+                    <p>Antonio Robin</p>
+                    <p>Email: TonyRobin@birdhouse.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/woodpecker.jpg">
+                <div class="container">
+                    <h2>Head of Construction</h2>
+                    <p>Harold Woodpecker</p>
+                    <p>Email: HWoodyWoodpecker@birdhouse.com</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>

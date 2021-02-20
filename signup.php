44f65817b77dbee0601435d9ad95517d216b9071
RavenDC2 <dr00002@mix.wvu.edu>
2021-02-19 22:53:58 -0500

    lab4-login

--- signup.php
+++ signup.php
@@ -9,7 +9,7 @@ require "includes/header.php"
             <div class="my-auto">
                 <div class="signup-form">
                     <form action="includes/signup-helper.php" method="post">
-                        <h1 class="h3 mb-3 font-weight-normal">Please sign up</h1>
+                        <h1 class="h3 mb-3 font-weight-normal">Please Sign Up</h1>
                         <p class="hint-text">Create your Account!</p>
                         <div class="form-group">
                             <div class="row">
@@ -30,7 +30,7 @@ require "includes/header.php"
                         <label for="inputPassword" class="sr-only">Confirm Password</label>
                         <input type="password" id="inputPassword" class="form-control" name="con-pwd" placeholder="Confirm Password"
                             required>
-                        <button class="btn btn-lg btn-outline-primary btn-block" name="signup-submit" type="submit">Sign in</button>
+                        <button class="btn btn-lg btn-outline-primary btn-block" name="signup-submit" type="submit">Sign Up</button>
                         <p class="mt-5 mb-3 text-muted">&copy; 2020-9999</p>
                     </form>
                 </div>
